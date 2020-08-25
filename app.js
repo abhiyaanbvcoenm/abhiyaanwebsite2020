fd6f786ad8683a53ee74124b44293be1760ee3b5
Divyesh <divyesh1099@gmail.com>
2020-08-25 10:17:03 +0530

    Second Commit

--- app.js
+++ app.js
@@ -11,13 +11,23 @@ app.get("/",(req,res)=>{
 /////////////////////////////////////////////////////////
 //News
 app.get("/news",(req,res)=>{
-    res.render("news/new");
+    res.render("news/news");
 });
 app.get("/news/blog",(req,res)=>{
     res.render("news/blog");
 });
 app.get("/news/magzine",(req,res)=>{
     res.render("news/magzine");
+});
+//News Curd
+app.get("/news/new",(req,res)=>{
+    res.render("news/new");
+});
+app.get("/news/show",(req,res)=>{
+    res.render("news/show");
+});
+app.get("/news/edit",(req,res)=>{
+    res.render("news/edit");
 });
 /////////////////////////////////////////////////////////
 //Days
@@ -36,6 +46,16 @@ app.get("/days/corporate",(req,res)=>{
 app.get("/days/traditional",(req,res)=>{
     res.render("days/traditional");
 });
+//Days Curd
+app.get("/days/new",(req,res)=>{
+    res.render("days/new");
+});
+app.get("/days/show",(req,res)=>{
+    res.render("days/show");
+});
+app.get("/days/edit",(req,res)=>{
+    res.render("days/edit");
+});
 /////////////////////////////////////////////////////////
 //Events
 app.get("/events",(req,res)=>{
@@ -44,6 +64,16 @@ app.get("/events",(req,res)=>{
 app.get("/events/event1",(req,res)=>{
     res.render("events/event1");
 });
+//Events Curd
+app.get("/events/new",(req,res)=>{
+    res.render("events/new");
+});
+app.get("/events/show",(req,res)=>{
+    res.render("events/show");
+});
+app.get("/events/edit",(req,res)=>{
+    res.render("events/edit");
+});
 /////////////////////////////////////////////////////////
 //Departmental Fests
 app.get("/departmentalfests",(req,res)=>{
@@ -51,6 +81,16 @@ app.get("/departmentalfests",(req,res)=>{
 });
 app.get("/departmentalfests/departmentalfest1",(req,res)=>{
     res.render("departmental fests/departmentalfest1");
+});
+//Departmental Fests Curd
+app.get("/departmentalfests/new",(req,res)=>{
+    res.render("departmental fests/new");
+});
+app.get("/departmentalfests/show",(req,res)=>{
+    res.render("departmental fests/show");
+});
+app.get("/departmentalfests/edit",(req,res)=>{
+    res.render("departmental fests/edit");
 });
 /////////////////////////////////////////////////////////
 //Login Signup Admin
@@ -62,6 +102,20 @@ app.get("/signup",(req,res)=>{
 });
 app.get("/admin",(req,res)=>{
     res.render("admin");
+});
+/////////////////////////////////////////////////////////
+//Footer links
+//Previous Events
+app.get("/previousevents",(req,res)=>{
+    res.render("footer links/previousevents");
+});
+//About Developers
+app.get("/aboutdevelopers",(req,res)=>{
+    res.render("footer links/aboutdevelopers");
+});
+//Award
+app.get("/awards",(req,res)=>{
+    res.render("footer links/awards");
 });
 /////////////////////////////////////////////////////////
 //Error Page Route
